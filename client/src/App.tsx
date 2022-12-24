191585c8d58874fea69fb8765ecc8e5350ac7e03
Andrew Lenhart <andrew.code21@gmail.com>
2022-12-24 12:55:53 -0500

    build-theme-context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { createContext, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type ButtonComponentProps = {
+  id?: string;
+  text?: string;
+  type?: "reset" | "submit" | "button";
+  value?: string | number;
+  onClick?: () => void;
+  disabled?: boolean;
+};
 
+const Button = ({
+  id = "",
+  text = "Button Text",
+  type = "button",
+  value = "",
+  onClick = () => console.log("You clicked a button!"),
+  disabled = false,
+}: ButtonComponentProps) => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <button
+      id={id}
+      type={type}
+      value={value}
+      onClick={onClick}
+      className="Button"
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+// interface State<T> {
+//   data?: T
+//   error?: Error
+// }
+// type Cache<T> = { [url: string]: T }
+// // discriminated union type
+// type Action<T> =
+//   | { type: 'loading' }
+//   | { type: 'fetched'; payload: T }
+//   | { type: 'error'; payload: Error }
+
+enum THEME_MODE {
+  light = "THEME-MODE-LIGHT",
+  dark = "THEME-MODE-DARK",
+  dim = "THEME-MODE-DIM",
 }
 
-export default App
+function useThemeMode() {
+  const themeStore = useState(THEME_MODE.dark);
+  return themeStore;
+}
+
+type ThemeStoreReturnType = ReturnType<typeof useThemeMode>;
+const ThemeContext = createContext({} as ThemeStoreReturnType);
+
+const ThemeContextProvider = ({ children }: { children: JSX.Element }) => {
+  const [theme, setTheme] = useThemeMode();
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// type ThemeActionType =
+// | { type: THEME_MODE.light, payload: THEME_MODE.light }
+// | { type: THEME_MODE.dark, payload: THEME_MODE.dark }
+// | { type: THEME_MODE.dim, payload: THEME_MODE.dim }
+
+// function themeReducer(state: ThemeStateInterface, action: ThemeActionType) {
+//   switch(action.type) {
+//     case
+//   }
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Title">Vite + React</h1>
+      <div className="card">
+        <p className="Text">Change Theme Mode</p>
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default App;
